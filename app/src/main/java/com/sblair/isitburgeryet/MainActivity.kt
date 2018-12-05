45d59746ed325e8bdbe8598daf5023d78647edaf
Shane Blair <sblair@wustl.edu>
2018-12-05 07:55:25 -0600

    recently searched

--- app/src/main/java/com/sblair/isitburgeryet/MainActivity.kt
+++ app/src/main/java/com/sblair/isitburgeryet/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.sblair.isitburgeryet
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -17,12 +20,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var recipeResultList: ArrayList<RecipeSearch>
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        sharedPreferences = getSharedPreferences("$packageName.${SharedPrefKeys.SORT_PREFERENCES_FILE}", Context.MODE_PRIVATE)
         val actionbar: ActionBar? = supportActionBar
         actionbar?.apply {
             setDisplayHomeAsUpEnabled(true)
@@ -41,6 +46,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         searchBtn.setOnClickListener {
+            var recents = sharedPreferences.getStringSet(SharedPrefKeys.recents, setOf(search.text.toString())) as Set<String>
+            var temp = HashSet(recents)
+            temp.add(search.text.toString())
+
+            val editor = sharedPreferences.edit()
+            editor.putStringSet(SharedPrefKeys.recents, temp)
+
+            editor.apply()
             object : Thread() {
                 override fun run() {
                     var create = search.text.toString()
@@ -60,6 +73,31 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         nav_view.menu.getItem(0).setChecked(true)
+        val recents = sharedPreferences.getStringSet(SharedPrefKeys.recents, setOf())
+        recentsList.removeAllViews()
+        for (item in recents) {
+            val recentText = TextView(this)
+            recentText.text = item
+            recentText.textSize = 20f
+            recentText.setTextColor(Color.BLUE)
+            recentText.isClickable = true
+            recentText.setOnClickListener {
+                object : Thread() {
+                    override fun run() {
+                        var create = recentText.text.toString()
+                        val resultList = QueryUtils.fetchRecipeData(create)
+
+                        if (resultList != null) {
+                            this@MainActivity.recipeResultList = resultList
+                            val intent = Intent(this@MainActivity, RecipeListActivity::class.java)
+                            intent.putExtra("RESULT_ARRAY", recipeResultList)
+                            startActivity(intent)
+                        }
+                    }
+                }.start()
+            }
+            recentsList.addView(recentText)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
---
+++ app/src/main/java/com/sblair/isitburgeryet/SharedPrefKeys.kt
@@ -0,0 +1,8 @@
+package com.sblair.isitburgeryet
+
+class SharedPrefKeys {
+    companion object {
+        const val SORT_PREFERENCES_FILE = ".sort_preferences"
+        const val recents = "RECENTS"
+    }
+}
